e0ea7ebc055b86f73bb896135c39f3c8bbf078a0
sahil-lihas <atpug.sahil@gmail.com>
2022-05-21 13:31:26 +0530

    docker files added

---
+++ api/build/precache-manifest.a778612061bfebbb8a721f47b0365699.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2902f80a24c144afde628c8bd69bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41d004b42d4026fea7d6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7e60079c214ae6212d93",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "41d004b42d4026fea7d6",
+    "url": "/static/js/2.a7850a21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a7850a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e60079c214ae6212d93",
+    "url": "/static/js/main.adb14745.chunk.js"
+  },
+  {
+    "revision": "a38d0a1b14f6dcf880f8",
+    "url": "/static/js/runtime-main.ad4adc77.js"
+  },
+  {
+    "revision": "09b2354814c61f76c1645944a393624c",
+    "url": "/static/media/Bread.09b23548.jpg"
+  },
+  {
+    "revision": "d6e3db42de68e38a5ad272f45147a65d",
+    "url": "/static/media/cheese.d6e3db42.jpeg"
+  },
+  {
+    "revision": "89e849f1698f8f82857e072981dc46bd",
+    "url": "/static/media/eggs.89e849f1.jpg"
+  },
+  {
+    "revision": "92678e66d363a5e6a97aa6730afcf933",
+    "url": "/static/media/milk.92678e66.jpg"
+  }
+]);
